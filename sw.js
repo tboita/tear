529f0b553df1232f0254931c676d770055f511ca
tboita <85522734+tboita@users.noreply.github.com>
2026-08-29 19:29:37 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tear-v2';
+const CACHE = 'tear-v3';
 const ARQUIVOS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png'];
 
 self.addEventListener('install', e => {
